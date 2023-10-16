1d23a84020b9f05fc4caa82adcd758008dd54e1a
Carlos Gomes <carlos_gomes1809@hotmail.com>
2023-10-16 17:34:56 -0300

    comment unused component from clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
-      <UserButton />
+      {/* <UserButton /> */}
 
       <section className="mt-9 flex flex-col gap-10">
         {result?.threads.length === 0 ? (
